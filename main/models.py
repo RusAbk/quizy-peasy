c76b657e2a471feb052e675d547e832affc4a79d
Ruslan Abkadirov <rusabk@yandex.ru>
2022-02-14 04:43:23 +0300

    models Quiz, Question, Answer added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Quiz(models.Model):
+    author = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField()
+    title = models.CharField(max_length=300)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(to=Quiz, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    image = models.ImageField(upload_to ='uploads/question_img/')
+
+class Answer(models.Model):
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=50)
